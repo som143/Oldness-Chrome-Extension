912a08a62c3491b50642556988b452a0873386f6
Hideto ISHIBASHI <hidetoi@gmail.com>
2014-11-13 22:58:15 +0900

    Variable badge background color.

--- background.js
+++ background.js
@@ -1,46 +1,66 @@
 var tabOldnessMap = {} // { integer tabId -> string oldness}
 
-function refreshBadge(tabId) {
+function sendRequest(tabId) {
   chrome.tabs.get(tabId, function (tab){
     waybackAPIRequestURL = "http://archive.org/wayback/available?timestamp=19010101&url=" + tab.url
-    timeout = 1000 // milliseconds
+    timeout = 5000 // milliseconds
     $.ajax({
       url: waybackAPIRequestURL,
       dataType: 'json',
       timeout: timeout,
-      success: function(data){
-        if (!data.archived_snapshots ||
-          !data.archived_snapshots.closest) {
+      success: function (ajaxResponse) {
+        if (!ajaxResponse.archived_snapshots ||
+        !ajaxResponse.archived_snapshots.closest) {
+          delete tabOldnessMap[tab.id]
           return
         }
-        meta = data.archived_snapshots.closest
+        meta = ajaxResponse.archived_snapshots.closest
         if (meta.available) {
-          oldness = moment(meta.timestamp, "YYYYMMDDhhmmss").fromNow()
+          timestamp = moment(meta.timestamp, "YYYYMMDDhhmmss")
+          oldness = timestamp.fromNow()
           tabOldnessMap[tab.id] = oldness
+          bgColor = badgeBackgroundColor(timestamp)
+          chrome.browserAction.setBadgeBackgroundColor({color: bgColor})
           chrome.browserAction.setBadgeText({
             text: oldness,
-            tabId: tabId})
+            tabId: tabId
+          })
         }
       }
     })
   })
 }
 
+function badgeBackgroundColor(timestamp) { // CSS RGB color or null
+  var now = moment()
+  months = now.diff(timestamp, "months", true)
+  if (months < 0.2) return [142,142,142,255]
+  if (months < 1) return [154,205,231,255]
+  if (months < 4) return [234,168,191,255]
+  if (months < 12) return [255,238,85,255]
+  return [218,80,25,255]
+}
+
 // event listeners
+
+chrome.tabs.onUpdated.addListener(function(tabId, props) {
+  sendRequest(tabId)
+})
 
 chrome.tabs.onRemoved.addListener(function(tabId, props) {
   delete tabOldnessMap[tabId]
 })
 
 chrome.tabs.onSelectionChanged.addListener(function(tabId, props) {
-  refreshBadge(tabId)
+  sendRequest(tabId)
 })
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-    if (!sender.tab && tabOldnessMap[request.tabId]) {
+    if (!sender.tab) {
       oldness = tabOldnessMap[request.tabId]
       sendResponse({
+        isAvailable: oldness ? true : false,
         oldness: oldness
       })
     }
@@ -50,5 +70,5 @@ chrome.runtime.onMessage.addListener(
 // setup
 
 chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-  refreshBadge(tabs[0].id)
+  sendRequest(tabs[0].id)
 })
--- popup.js
+++ popup.js
@@ -1,7 +1,25 @@
+var tab;
+
+function refreshPoup() {
+  $("#waybackLink").attr("href", "http://wayback.archive.org/web/*/" + tab.url)
+  chrome.runtime.sendMessage({tabId: tab.id}, function(response) {
+    if (response.isAvailable) {
+      $("#oldness").text(response.oldness)
+    } else {
+      $("#oldness").text("Not Available")
+    }
+  })
+}
+
+// setup
+
 chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-  tab = tabs[0]
-	chrome.runtime.sendMessage({tabId: tab.id}, function(response) {
-	  $("#oldness").text(response.oldness)
-    $("#waybackLink").attr("href", "http://wayback.archive.org/web/*/" + tab.url)
-	})
+    tab = tabs[0]
+    refreshPoup()
+})
+
+chrome.tabs.onUpdated.addListener(function(tabId, props) {
+  if (tabId == tab.id) {
+    refreshPoup()
+  }
 })
